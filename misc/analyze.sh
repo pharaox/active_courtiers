b24d63c14f9a96f7eeb3236b9e406b4a1c2a1fe6
pharaox <pharaox@tutanota.com>
2024-01-23 18:07:37 +0200

    Add analyze.sh

---
+++ misc/analyze.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Marriage events
+echo "accou_marriage.0001 event option a: $(grep -a "accou_marriage.0001 event option a" debug.log | wc -l)"
+echo "accou_marriage.0002 event option a: $(grep -a "accou_marriage.0002 event option a" debug.log | wc -l)"
+echo "accou_marriage.0002 event option b: $(grep -a "accou_marriage.0002 event option b" debug.log | wc -l)"
+pcre2grep -a -o1 "candidate quality: ([\d]+)" debug.log | awk '{s+=$1}END{print "average candidate quality:",s/NR}' RS="\n"
+echo
+
