ec1bec706d1d626f06427133f84ccd832dc1be93
pharaox <pharaox@tutanota.com>
2024-01-28 13:58:30 +0200

    Add game rules

--- misc/analyze.sh
+++ misc/analyze.sh
@@ -2,6 +2,10 @@
 
 # Marriage events
 echo "accou_marriage.0001 event option a: $(grep -a "accou_marriage.0001 event option a" debug.log | wc -l)"
+echo "- success: $(grep -a "accou_marriage.0001 event option a success" debug.log | wc -l)"
+echo "- failure: $(grep -a "accou_marriage.0001 event option a failure" debug.log | wc -l)"
+echo "- failure fp: $(pcre2grep -a "accou_marriage.0001 event option a failure, courtier: .+ \([\d]+\), candidate: .+ \([\d]+\), matchmaker: .+ \([\d]+\)" debug.log | wc -l)"
+echo
 echo "accou_marriage.0002 event option a: $(grep -a "accou_marriage.0002 event option a" debug.log | wc -l)"
 echo "- outsider: $(grep -a "accou_marriage.0002 event option a, .*, outsider: yes" debug.log | wc -l)"
 echo "- insider: $(grep -a "accou_marriage.0002 event option a, .*, outsider: no" debug.log | wc -l)"
