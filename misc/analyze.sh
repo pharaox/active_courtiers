aac39473f880ceb78de750c0967e7d5d6f811a74
pharaox <pharaox@tutanota.com>
2024-04-06 18:38:32 +0300

    Rename game rule to "Interaction Acceptance by AI"

--- misc/analyze.sh
+++ misc/analyze.sh
@@ -14,15 +14,19 @@ echo "- insider: $(grep -a "accou_marriage.0002 event option a, .*, outsider: no
 echo "accou_marriage.0002 event option b: $(grep -a "accou_marriage.0002 event option b" debug.log | wc -l)"
 echo "- outsider: $(grep -a "accou_marriage.0002 event option b, .*, outsider: yes" debug.log | wc -l)"
 echo "- insider: $(grep -a "accou_marriage.0002 event option b, .*, outsider: no" debug.log | wc -l)"
-pcre2grep -a -o1 "candidate quality: ([\d]+)" debug.log | awk '{s+=$1}END{print "average candidate quality:",s/NR}' RS="\n"
+pcre2grep -a -o1 "accou_marriage.0002 event .* candidate quality: ([\d]+)" debug.log | awk '{s+=$1}END{print "average marriage candidate quality:",s/NR}' RS="\n"
 echo
 
 # Invite events
 echo "accou_invite.0001 event option a: $(grep -a "accou_invite.0001 event option a" debug.log | wc -l)"
 echo "- success: $(grep -a "accou_invite.0001 event option a success" debug.log | wc -l)"
 echo "- failure: $(grep -a "accou_invite.0001 event option a failure" debug.log | wc -l)"
-echo "  - no candidate: $(pcre2grep -a "accou_invite.0001 event option a failure, candidate:  \(4294967295\)" debug.log | wc -l)"
+#echo "  - no candidate: $(pcre2grep -a "accou_invite.0001 event option a failure, candidate:  \(4294967295\)" debug.log | wc -l)"
 echo "- failure fp: $(pcre2grep -a "accou_invite.0001 event option a failure, candidate: .+ \([\d]+\), courtier: .+ \([\d]+\)" debug.log | wc -l)"
+echo
+echo "accou_invite.0002 event option a: $(grep -a "accou_invite.0002 event option a" debug.log | wc -l)"
+echo "accou_invite.0002 event option b: $(grep -a "accou_invite.0002 event option b" debug.log | wc -l)"
+pcre2grep -a -o1 "accou_invite.0002 event .* candidate quality: ([\d]+)" debug.log | awk '{s+=$1}END{print "average invite candidate quality:",s/NR}' RS="\n"
 echo
 
 # Courtier events
